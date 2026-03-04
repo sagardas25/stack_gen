bda334dee76a037fdc4f0100ef51e87ca3804bad
sagardas25 <sagar.xyz05@gmail.com>
2026-03-04 22:57:10 +0530

    feat(tanstack) : implemented tanstack-query

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { QueryProvider } from "@/components/ui/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,7 @@ export default function RootLayout({ children }) {
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <QueryProvider>{children}</QueryProvider>
           </ThemeProvider>
         </body>
       </html>
---
+++ src/modules/projects/hooks/project.js
@@ -0,0 +1,29 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  createProject,
+  getProjectById,
+  getProjects,
+} from "../actions/index.js";
+import { queryKeys } from "inngest";
+
+export const useGetProjects = () => {
+  return useQuery({
+    queryKey: ["projects"],
+    queryFn: () => getProjects(),
+  });
+};
+
+export const useCreateProject = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (value) => createProject(value),
+    onSuccess: queryClient.invalidateQueries(["projects"]),
+  });
+};
+
+export const useGetProjectsById = (projectId) => {
+  return {
+    queryKey: ["project", projectId],
+    queryFn: () => getProjectById(projectId),
+  };
+};
